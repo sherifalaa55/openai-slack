d09f5bc02fc9f4a100e887495da06079746d0f90
Sherif Alaa <sherif.alaa55@gmail.com>
2024-05-16 07:32:01 +0300

    added logs

--- api/events.ts
+++ api/events.ts
@@ -22,7 +22,7 @@ export async function POST(request: Request) {
   const rawBody = await request.text()
   const body = JSON.parse(rawBody)
   const requestType = body.type
-
+  console.log(requestType)
   if (requestType === 'url_verification') {
     return new Response(body.challenge, { status: 200 })
   }
